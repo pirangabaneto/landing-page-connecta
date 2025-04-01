3bba26318bf9d9c0edb85a08255c4c21fe30c081
pirangaba <pirangabaneto@gmail.com>
2025-04-01 12:40:33 -0300

    adiciona framer-motion para animações

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,14 +1,34 @@
+import { motion } from "framer-motion";
+import { useRef } from "react";
+import { useInView } from "framer-motion";
+
 const AboutMe = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { triggerOnce: true, threshold: 0.3 });
+
   return (
-    <section className="flex flex-row items-center justify-center gap-10 bg-[#3570AB] w-screen min-h-screen">
-      <div>
+    <section
+      ref={ref}
+      className="flex flex-row items-center justify-center gap-10 bg-[#3570AB] w-screen min-h-screen"
+    >
+      <motion.div
+        initial={{ opacity: 0, x: -200 }}
+        animate={isInView ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 3 }}
+      >
         <img
           src="/dentista-02.jpg"
           alt="Foto de perfil"
           className="rounded-4xl"
         />
-      </div>
-      <div className=" text-white p-10 w-1/2">
+      </motion.div>
+
+      <motion.div
+        className="text-white p-10 w-1/2"
+        initial={{ opacity: 0, x: 200 }}
+        animate={isInView ? { opacity: 1, x: 0 } : {}}
+        transition={{ duration: 3 }}
+      >
         <p className="text-6xl font-semibold">Sobre mim</p>
         <p className="my-8 mt-20">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
@@ -19,7 +39,7 @@ const AboutMe = () => {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
-      </div>
+      </motion.div>
     </section>
   );
 };
